47b9493db9537e6625a69c7f7bb7cadc817d7447
Daniel Van Noord <sg1mash98@gmail.com>
2021-10-03 21:04:33 -0400

    Added code from ASCOM Cross Platform libraries

---
+++ TraceLoggerCompat.cs
@@ -0,0 +1,47 @@
+﻿using ASCOM.Common;
+using ASCOM.Common.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASCOM.Compatibility.Utilities
+{
+    public class TraceLoggerCompat : ASCOM.Utilities.TraceLogger, ILogger
+    {
+        public TraceLoggerCompat() : base()
+        {
+
+        }
+
+        public TraceLoggerCompat(string LogFileType) : base(LogFileType)
+        {
+
+        }
+
+        public TraceLoggerCompat(string LogFileName, string LogFileType) : base(LogFileName, LogFileType)
+        {
+
+        }
+
+        public LogLevel LoggingLevel
+        {
+            get;
+            private set;
+        } = LogLevel.Information;
+
+        public void Log(LogLevel level, string message)
+        {
+            if (this.IsLevelActive(level))
+            {
+                this.LogMessage($"[{level}]", message);
+            }
+        }
+
+        public void SetMinimumLoggingLevel(LogLevel level)
+        {
+            LoggingLevel = level;
+        }
+    }
+}
